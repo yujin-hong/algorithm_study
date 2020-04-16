7fc1f5fe969b2d3343308674662477047460b4d7
yujin hong <dbwlsdl2500@gist.ac.kr>
2020-04-16 12:32:04 +0900

    fix 13460

--- 13460/main.cpp
+++ 13460/main.cpp
@@ -4,8 +4,9 @@
 using namespace std;
 
 int visited[11][11];
+int check[11][11][11][11];
 
-queue<pair<int, int>> q;
+queue<pair<int, int>> rq;
 queue<pair<int, int>> bq;
 
 int dx[4]={-1, 0, 1, 0};
@@ -26,68 +27,77 @@ int main()
             char ch;
             cin>>ch;
             if(ch=='#') visited[i][j]=-1;
-            else if(ch=='R') q.push(make_pair(i,j));
-            else if(ch=='B') {visited[i][j]=-3; bq.push(make_pair(i, j));}
+            else if(ch=='R') rq.push(make_pair(i,j));
+            else if(ch=='B') bq.push(make_pair(i,j));
             else if(ch=='O') visited[i][j]=-2;
         }
     }
-//    for(int i=0;i<n;i++) {
-//        for(int j=0;j<m;j++) {
-//            cout<<visited[i][j]<<' ';
-//        }
-//        cout<<'\n';
-//    }
+
     int num=1;
     int ans=0;
-    while(!q.empty()) {
-        for(int i=0;i<q.size();i++) {
-            int qx = q.front().first;
-            int qy = q.front().second;
+    while(!rq.empty() && !bq.empty()) {
+//        if(num>10) {ans=-1; break;}
+        int rqsize = rq.size();
+        cout<<"==================== "<<rqsize<<'\n';
+
+        for(int i=0;i<rqsize;i++) {
+            int rx = rq.front().first;
+            int ry = rq.front().second;
             int bx = bq.front().first;
             int by = bq.front().second;
-//            cout<<qx<<" "<<qy<<" "<<bx<<" "<<by<<'\n';
-
-            q.pop();
+            cout<<rx<<" "<<ry<<" "<<bx<<" "<<by<<'\n';
+            rq.pop();
             bq.pop();
             for(int j=0;j<4;j++) {
-                int tx = qx+dx[j];
-                int ty = qy+dy[j];
-//                cout<<"tx: "<<tx<<" "<<ty<<'\n';
-                if(visited[tx][ty]==0 || visited[tx][ty]==-2) {
-//                    cout<<"tx1: "<<tx<<" "<<ty<<'\n';
-                    while(1) {
-                        bx=bx+dx[j];
-                        by=by+dy[j];
-//                        cout<<"bx: "<<bx<<" "<<by<<" "<<visited[bx][by]<<'\n';
-                        if(visited[bx][by]!=0) break;
-                    }
-//                    visited[bx-dx[j]][by-dy[j]]=-3;
-//                    visited[tx][ty]=num;
-                    while(1) {
-                        if(visited[tx][ty]!=0) break;
-                        else visited[tx][ty]=num;
-                        tx=tx+dx[j];
-                        ty=ty+dy[j];
-                    }
-
-                    if(visited[tx][ty]==-2) {
-//                            cout<<"hohohoho"<<'\n';
-                        if(bx==tx && by==ty) {
-                            ans=-1;
-                        } else {
-                            ans=num;
-                        }
-                        break;
-                    }
-                    if(bx==tx && by==ty) { bx=bx-dx[j]; by=by-dy[j]; }
-//                    else if(visited[tx][ty]==-3) {ans=-1; break;}
-//                    cout<<"it will go: b: "<<bx-dx[j]<<" "<<by-dy[j]<<'\n';
-//                    cout<<"it will go: r: "<<tx-dx[j]<<" "<<ty-dy[j]<<'\n';
-
-                    q.push(make_pair(tx-dx[j], ty-dy[j]));
-                    bq.push(make_pair(bx-dx[j], by-dy[j]));
-//                    visited[bx-dx[j]][by-dy[j]]=0;
+                int bfl=0;
+                int ttbx=bx;
+                int ttby=by;
+                int ttrx=rx;
+                int ttry=ry;
+                while(1) {
+                    ttbx=ttbx+dx[j];
+                    ttby=ttby+dy[j];
+                    if(ttbx==rx && ttby==ry) bfl=1;
+                    if(visited[ttbx][ttby]!=0) break;
                 }
+                int rfl=0;
+                while(1) {
+                    ttrx=ttrx+dx[j];
+                    ttry=ttry+dy[j];
+                    if(bx==ttrx && by==ttry) rfl=1;
+                    if(visited[ttrx][ttry]!=0) break;
+                }
+                cout<<"tt"<<ttrx<<" "<<ttry<<" "<<ttbx<<" "<<ttby<<'\n';
+
+                if(visited[ttrx][ttry]==-2) {
+                    if(visited[ttrx][ttry]==visited[ttbx][ttby]) continue;
+                    else {ans=num; break;}
+                } else if(visited[ttbx][ttby]==-2) {
+                    continue;
+                } else {
+//                    cout<<"go to queue"<<'\n';
+                    if(check[ttrx-(rfl+1)*dx[j]][ttry-(rfl+1)*dy[j]][ttbx-(bfl+1)*dx[j]][ttby-(bfl+1)*dy[j]]==0) {
+                        bq.push(make_pair(ttbx-(bfl+1)*dx[j], ttby-(bfl+1)*dy[j]));
+                        rq.push(make_pair(ttrx-(rfl+1)*dx[j], ttry-(rfl+1)*dy[j]));
+                        check[ttrx-(rfl+1)*dx[j]][ttry-(rfl+1)*dy[j]][ttbx-(bfl+1)*dx[j]][ttby-(bfl+1)*dy[j]]=1;
+                    }
+                }
+                cout<<rq.size()<<'\n';
+
+//                    if(visited[tx][ty]==-2) {
+////                            cout<<"hohohoho"<<'\n';
+//                        if(bx==tx && by==ty) {
+//                            ans=-1;
+//                        } else {
+//                            ans=num;
+//                        }
+//                        break;
+//                    }
+//                    if(bx==tx && by==ty) { bx=bx-dx[j]; by=by-dy[j]; }
+
+
+//                    q.push(make_pair(tx-dx[j], ty-dy[j]));
+//                }
             }
 //            cout<<"nowans: "<<ans<<'\n';
             if(ans!=0) break;
@@ -96,7 +106,7 @@ int main()
         num++;
     }
 //    cout<<"origin: "<<ans<<'\n';
-    if(ans>10 || ans==0) ans=-1;
-    cout << ans << '\n';
+    if(ans==0) ans=-1;
+    cout <<ans << '\n';
     return 0;
 }
