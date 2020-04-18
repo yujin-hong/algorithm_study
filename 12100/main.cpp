569554d16f5c72499b1becf06bcf10a984eaf72a
yujin hong <dbwlsdl2500@gist.ac.kr>
2020-04-18 18:05:18 +0900

    solve 12100

--- 12100/main.cpp
+++ 12100/main.cpp
@@ -11,7 +11,7 @@ int dy[4]={0, 1, 0, -1};
 int nmax=0;
 
 void go(int tarr[21][21], int n, int dep) {
-    cout<<"==================="<<dep<<'\n';
+//    cout<<"==================="<<dep<<'\n';
     if(dep==5) {
         for(int i=0;i<n;i++) {
             for(int j=0;j<n;j++) {
@@ -29,17 +29,67 @@ void go(int tarr[21][21], int n, int dep) {
         }
 //        int starti, startj, endi, endj;
         int fl=0;
-        if(k==0 || k==3) {
+        if(k==0) {
+            for(int i=0;i<n;i++) {
+                for(int j=0;j<n;j++) {
+                    if(arr[j][i]!=0) {
+                        int tmp=arr[j][i];
+//                        cout<<"here"<<i<<" "<<j<<" "<<tmp<<'\n';
+                        arr[j][i]=0;
+                        int tx=j, ty=i;
+    //                    cout<<"tx: "<<tx<<" "<<ty<<'\n';
+                        while(tx>=0 && ty>=0 && tx<n && ty<n && arr[tx][ty]==0) {
+//                            cout<<"while tx: "<<tx<<" "<<ty<<'\n';
+                            tx=tx+dx[k];
+                            ty=ty+dy[k];
+    //                        cout<<tx<<" "<<ty<<'\n';
+                        }
+                        if(tx>=0 && ty>=0 && tx<n && ty<n && arr[tx][ty]==tmp && fl==0) {
+                            arr[tx][ty]=tmp*2;
+                            fl=1;
+                        } else {
+                            arr[tx-dx[k]][ty-dy[k]]=tmp;
+                            fl=0;
+                        }
+                    }
+                }
+            }
+        } else if (k==3) {
             for(int i=0;i<n;i++) {
                 for(int j=0;j<n;j++) {
                     if(arr[i][j]!=0) {
                         int tmp=arr[i][j];
-                        cout<<"here"<<i<<" "<<j<<" "<<tmp<<'\n';
+//                        cout<<"here"<<i<<" "<<j<<" "<<tmp<<'\n';
                         arr[i][j]=0;
                         int tx=i, ty=j;
     //                    cout<<"tx: "<<tx<<" "<<ty<<'\n';
                         while(tx>=0 && ty>=0 && tx<n && ty<n && arr[tx][ty]==0) {
-                            cout<<"while tx: "<<tx<<" "<<ty<<'\n';
+//                            cout<<"while tx: "<<tx<<" "<<ty<<'\n';
+                            tx=tx+dx[k];
+                            ty=ty+dy[k];
+    //                        cout<<tx<<" "<<ty<<'\n';
+                        }
+                        if(tx>=0 && ty>=0 && tx<n && ty<n && arr[tx][ty]==tmp && fl==0) {
+                            arr[tx][ty]=tmp*2;
+                            fl=1;
+                        } else {
+                            arr[tx-dx[k]][ty-dy[k]]=tmp;
+                            fl=0;
+                        }
+                    }
+                }
+            }
+        } else if(k==2) {
+            for(int i=n-1;i>=0;i--) {
+                for(int j=n-1;j>=0;j--) {
+                    if(arr[j][i]!=0) {
+                        int tmp=arr[j][i];
+//                        cout<<"here"<<i<<" "<<j<<" "<<tmp<<'\n';
+                        arr[j][i]=0;
+                        int tx=j, ty=i;
+    //                    cout<<"tx: "<<tx<<" "<<ty<<'\n';
+                        while(tx>=0 && ty>=0 && tx<n && ty<n && arr[tx][ty]==0) {
+//                            cout<<"while tx: "<<tx<<" "<<ty<<'\n';
                             tx=tx+dx[k];
                             ty=ty+dy[k];
     //                        cout<<tx<<" "<<ty<<'\n';
@@ -81,14 +131,14 @@ void go(int tarr[21][21], int n, int dep) {
             }
         }
 
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) {
-                cout<<arr[i][j]<<" ";
-            }
-            cout<<'\n';
-        }
-        cout<<"============"<<'\n';
-//        go(arr, n, dep+1);
+//        for(int i=0;i<n;i++) {
+//            for(int j=0;j<n;j++) {
+//                cout<<arr[i][j]<<" ";
+//            }
+//            cout<<'\n';
+//        }
+//        cout<<"============"<<'\n';
+        go(arr, n, dep+1);
     }
 }
 
